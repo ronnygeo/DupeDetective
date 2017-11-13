edd9006f2c483e3f519d96986781d8d751a9268b
Ronny Mathew <ronnygeo@ccs.neu.edu>
2017-11-13 16:04:21 -0500

    MD5Parser support to check string.

--- PhaseC/src/main/java/cs5500/project/engine/md5/MD5Parser.java
+++ PhaseC/src/main/java/cs5500/project/engine/md5/MD5Parser.java
@@ -3,17 +3,27 @@ package cs5500.project.engine.md5;
 import java.io.File;
 import java.io.IOException;
 
+import com.google.common.base.Charsets;
 import com.google.common.hash.HashCode;
+import com.google.common.hash.HashFunction;
 import com.google.common.hash.Hashing;
 
 public class MD5Parser {
 
-    public String getMD5File(String filename) {
+    public static String getMD5File(String filename) {
         try {
             HashCode hash = com.google.common.io.Files.hash(new File(filename), Hashing.md5());
             return hash.toString();
         } catch (IOException io) {
             return null;
         }
+    }
+
+    public static String getMD5String(String text) {
+        HashFunction hf = Hashing.md5();
+        HashCode hc = hf.newHasher()
+                        .putString(text, Charsets.UTF_8)
+                        .hash();
+        return hc.toString();
     }
 }
--- PhaseC/src/test/java/engine/md5/tests/Md5Tests.java
+++ PhaseC/src/test/java/engine/md5/tests/Md5Tests.java
@@ -32,4 +32,28 @@ public class Md5Tests {
         MD5Parser md5 = new MD5Parser();
         assertNotEquals(md5.getMD5File(file1), md5.getMD5File(file2));
     }
+
+    @Test
+    public void testMd5ValueString() {
+        String file = "Hi, Im the test file to check my MD5. I have a twin.";
+        MD5Parser md5 = new MD5Parser();
+        assertEquals("cba8589898ec23aab33e2eba90bad873", md5.getMD5String(file));
+    }
+
+    @Test
+    public void testMd5SameString() {
+        String file1 = "Hi, Im the test file to check my MD5. I have a twin.";
+        String file2 = "Hi, Im the test file to check my MD5. I have a twin.";
+        MD5Parser md5 = new MD5Parser();
+        assertEquals(md5.getMD5String(file1), md5.getMD5String(file2));
+    }
+
+    @Test
+    public void testMd5DiffString() {
+        String file1 = "Hi, Im the test file to check my MD5. I have a twin.";
+        String file2 = "Hi, Im a different test file to check my MD5.";
+        MD5Parser md5 = new MD5Parser();
+        assertNotEquals(md5.getMD5String(file1), md5.getMD5String(file2));
+    }
+
 }
