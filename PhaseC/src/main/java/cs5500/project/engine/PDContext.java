8077c30241b1cf886fd24a4b06fb48d8f1795e65
nikhila <nikhilaraya@ccs.neu.edu>
2017-11-18 16:03:14 -0500

    changes in pom.xml and created PDContext and XYZstrategy

---
+++ PhaseC/src/main/java/cs5500/project/engine/PDContext.java
@@ -0,0 +1,9 @@
+package cs5500.project.engine;
+
+public class PDContext {
+	private PDStrategy pdStrategyObj;
+	
+	public void setStrategy(PDStrategy psObj){
+		
+	}
+}
---
+++ PhaseC/src/main/java/cs5500/project/engine/XYZStrategy.java
@@ -0,0 +1,6 @@
+package cs5500.project.engine;
+
+public class XYZStrategy {
+	private Detector detect;
+	
+}
