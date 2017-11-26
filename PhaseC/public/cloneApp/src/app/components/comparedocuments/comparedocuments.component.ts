42fe36d16c40afe95dabbd387ed58376ff468fff
Ronny Mathew <ronnygeo@ccs.neu.edu>
2017-11-25 23:56:44 -0500

    Updates to report and compare docs.

--- PhaseC/public/cloneApp/src/app/components/comparedocuments/comparedocuments.component.ts
+++ PhaseC/public/cloneApp/src/app/components/comparedocuments/comparedocuments.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {DOC1} from "../../models/document1";
 import {DOC2} from "../../models/document2";
 import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
+import {SubmissionService} from "../../services/submission.service";
 
 @Component({
   selector: 'app-comparedocuments',
@@ -11,16 +12,21 @@ import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
 export class ComparedocumentsComponent implements OnInit {
 
   @Input() model;
+  @Input() refFileId;
+  @Input() similarFileId;
 
   private doc1: string[];
   private doc2: string[];
 
-  constructor(public activeModal: NgbActiveModal) { }
+  constructor(public activeModal: NgbActiveModal,
+              private submissionService: SubmissionService) { }
 
   ngOnInit() {
-    console.log(this.model);
-    this.doc1 = DOC1.split("\n");
-    this.doc2 = DOC2.split("\n");
+    console.log(this.model, this.refFileId, this.similarFileId);
+    this.submissionService.getAllSubmissions().subscribe(submissions => {
+      this.doc1 = submissions.filter(s => this.refFileId === s["id"]).map(s => s.filecontent)[0].split("\n");
+      this.doc2 = submissions.filter(s => this.similarFileId === s["id"]).map(s => s.filecontent)[0].split("\n");
+    });
   }
 
 
