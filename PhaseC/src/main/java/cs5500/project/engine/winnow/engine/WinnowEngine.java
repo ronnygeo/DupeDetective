4b56602b737aa4ce497eeb3a1323412d6ce84f9c
Joyal Shah <shah.jo@husky.neu.edu>
2017-11-29 19:37:45 -0500

    Hash Code Change

--- PhaseC/src/main/java/cs5500/project/engine/winnow/engine/WinnowEngine.java
+++ PhaseC/src/main/java/cs5500/project/engine/winnow/engine/WinnowEngine.java
@@ -34,36 +34,6 @@ public class WinnowEngine {
    * @return Hash of file
    */
   private LinkedHashMap calculateHash() {
-    int k = GlobalConstants.K_GRAM;
-    int q = GlobalConstants.PRIME;
-    int radix = 256;
-    int line_number = 1;
-
-    int highOrder = (int) (Math.pow(radix, k - 1) % q);
-
-    String[] lines = content.split("\n");
-
-    int index = 0;
-
-    int line_count = 1;
-
-    for (String line : lines) {
-      // this was a blank line and is only useful for the purpose of maintaining line number
-      // count, the text '$' has otherwise no other significance.
-      if (line.contains("$")) {
-        line_count += 1;
-        continue;
-      }
-
-      //calc length of line
-      int line_length = line.length();
-
-      HashRange index_range = new HashRange(index, index + line.length() - 1);
-      span_store.add(new HashIndex(index_range, line_count));
-      index = index + line_length;
-      line_count += 1;
-    }
-
     return fingerPrint;
   }
 
