6722fc3313a46d7a4c2bef8fe7d58e03f1ef82e0
nikhila <nikhilaraya@ccs.neu.edu>
2017-12-02 05:11:00 -0500

    controller tests

---
+++ PhaseC/server/src/test/java/engine/controllers/tests/ControllerTests.java
@@ -0,0 +1,81 @@
+package engine.controllers.tests;
+
+import static org.junit.Assert.*;
+
+import java.awt.List;
+import java.util.ArrayList;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.data.annotation.Id;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import cs5500.project.data.Assignment;
+import cs5500.project.web.controllers.AssignmentController;
+import cs5500.project.web.repository.AssignmentRepository;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(value = AssignmentController.class, secure = false)
+public class ControllerTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@MockBean
+	private AssignmentRepository assignRepo;
+	
+	
+	Assignment assign = new Assignment("1","ProblemSet1","CS5500",2017,true,"12/2/17","1/2/17","1/2/17");
+	java.util.List<Assignment> listOfAssignments = new ArrayList<Assignment>();
+	
+	
+	String exampleAssignmentJson = "{\"id\":\"1\",\"name\":\"ProblemSet1\",\"course\":\"CS5500\",\"year\":2017,\"isAnalyzed\":true,\"dueDate\":\"12/2/17\",\"creationDate\":\"1/2/17\",\"analyzedDate\":\"1/2/17\"}";
+	
+	@Test
+	public void getAssignmentByIdTest() throws Exception{
+		Mockito.when(
+				assignRepo.findOne(Mockito.anyString())).thenReturn(assign);
+
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.get(
+				"/assignments/1").accept(
+				MediaType.APPLICATION_JSON);
+
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+
+		System.out.println(result.getResponse());
+		String expected = "{id:1,name:ProblemSet1,course:CS5500}";
+
+		JSONAssert.assertEquals(expected, result.getResponse()
+				.getContentAsString(), false);
+	}
+
+	@Test
+	public void getAllAssignments() throws Exception{
+		Mockito.when(
+				assignRepo.findAll()).thenReturn(listOfAssignments);
+
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.get(
+				"/assignments").accept(
+				MediaType.APPLICATION_JSON);
+
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+
+		System.out.println(result.getResponse());
+		String expected = "{}";
+
+		JSONAssert.assertEquals(expected, result.getResponse()
+				.getContentAsString(), false);
+	}
+}
