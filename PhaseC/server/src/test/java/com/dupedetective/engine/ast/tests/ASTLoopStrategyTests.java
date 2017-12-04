64aa02fbb9ce3c231b816ffff1cc598e9f4867dd
shalinsitwala <shalin.sitwala@gmail.com>
2017-12-04 16:12:05 -0500

    adding tests for PDContext

--- PhaseC/server/src/test/java/com/dupedetective/engine/ast/tests/ASTLoopStrategyTests.java
+++ PhaseC/server/src/test/java/com/dupedetective/engine/ast/tests/ASTLoopStrategyTests.java
@@ -3,22 +3,41 @@ package com.dupedetective.engine.ast.tests;
 import static org.junit.Assert.*;
 
 import com.dupedetective.engine.ASTLoopStrategy;
+import com.dupedetective.engine.ASTStructureStrategy;
 import com.dupedetective.engine.PDContext;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * AST Loop Strategy Tests
  */
 public class ASTLoopStrategyTests {
-    @Test
-    public void TestLoopStrategy() {
-        String testCode1 = "\n public class A {" +
+
+    private PDContext pdContext;
+    private String testCode1, testCode2;
+
+    @Before
+    public void init() {
+        pdContext = new PDContext(new ASTLoopStrategy());
+        testCode1 = "\n public class A {" +
                 "\npublic void forLoop(int[] arr) { \n " +
                 "for(int j = 0; j < arr.length; j++) {i = arr[j];}} \n} ";
-        String testCode2 = "\n public class A {" +
+        testCode2 = "\n public class A {" +
                 "\npublic void forLoop(int[] arr) { \n " +
                 "for(int num : arr) {i = num;}} \n} ";
-        PDContext loop = new PDContext(new ASTLoopStrategy());
-        assertEquals(loop.executeStrategy(testCode1, testCode2).get(0).getScore(), 0.4, 0.01);
+    }
+
+    @Test
+    public void TestLoopStrategy() {
+
+        assertEquals(pdContext.executeStrategy(testCode1, testCode2).get(0).getScore(), 0.4, 0.01);
+    }
+
+    @Test
+    public void testMultipleStrategies() {
+        // Changing strategy to Structure Strategy
+        pdContext.setStrategy(new ASTStructureStrategy());
+        assertEquals(pdContext.executeStrategy(testCode1, testCode2).get(0).getScore(), 0.5, 0.1);
+
     }
 }
