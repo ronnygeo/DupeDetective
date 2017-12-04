e9a27842dc42eeae563d9d8d3424dc7df3baefcd
shalinsitwala <shalin.sitwala@gmail.com>
2017-12-04 16:42:34 -0500

    WinnowStrategy Test Fix

--- PhaseC/server/src/test/java/com/dupedetective/engine/winnow/tests/WinnowStrategyTests.java
+++ PhaseC/server/src/test/java/com/dupedetective/engine/winnow/tests/WinnowStrategyTests.java
@@ -2,6 +2,7 @@ package com.dupedetective.engine.winnow.tests;
 
 import com.dupedetective.engine.ASTLoopStrategy;
 import com.dupedetective.engine.PDContext;
+import com.dupedetective.engine.WinnowStrategy;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,7 +14,7 @@ public class WinnowStrategyTests {
 
     @Before
     public void init() {
-        pdContext = new PDContext(new ASTLoopStrategy());
+        pdContext = new PDContext(new WinnowStrategy());
         testCode1 = "\n public class A {" +
                 "\npublic void forLoop(int[] arr) { \n " +
                 "for(int j = 0; j < arr.length; j++) {i = arr[j];}} \n} ";
@@ -24,6 +25,6 @@ public class WinnowStrategyTests {
 
     @Test
     public void testWinnowStrategy() {
-        assertEquals(pdContext.executeStrategy(testCode1, testCode2).get(0).getScore(), 0.4, 0.001);
+        assertEquals(pdContext.executeStrategy(testCode1, testCode2).get(0).getScore(), 0, 0.001);
     }
 }
