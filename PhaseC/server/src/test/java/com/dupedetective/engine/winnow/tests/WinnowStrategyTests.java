910342d489403ffddf5bf9e9e05961b7ec42807f
shalinsitwala <shalin.sitwala@gmail.com>
2017-12-04 16:32:22 -0500

    WinnowStrategy Tests

---
+++ PhaseC/server/src/test/java/com/dupedetective/engine/winnow/tests/WinnowStrategyTests.java
@@ -0,0 +1,29 @@
+package com.dupedetective.engine.winnow.tests;
+
+import com.dupedetective.engine.ASTLoopStrategy;
+import com.dupedetective.engine.PDContext;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class WinnowStrategyTests {
+    private PDContext pdContext;
+    private String testCode1, testCode2;
+
+    @Before
+    public void init() {
+        pdContext = new PDContext(new ASTLoopStrategy());
+        testCode1 = "\n public class A {" +
+                "\npublic void forLoop(int[] arr) { \n " +
+                "for(int j = 0; j < arr.length; j++) {i = arr[j];}} \n} ";
+        testCode2 = "\n public class A {" +
+                "\npublic void forLoop(int[] arr) { \n " +
+                "for(int num : arr) {i = num;}} \n} ";
+    }
+
+    @Test
+    public void testWinnowStrategy() {
+        assertEquals(pdContext.executeStrategy(testCode1, testCode2).get(0).getScore(), 0.4, 0.001);
+    }
+}
