550efd47cb3aec33d7ee5ef0a911808202731dba
Joyal Shah <shah.jo@husky.neu.edu>
2017-12-04 16:44:36 -0500

    Updated Winnow Strategy

--- PhaseC/server/src/main/java/com/dupedetective/engine/WinnowStrategy.java
+++ PhaseC/server/src/main/java/com/dupedetective/engine/WinnowStrategy.java
@@ -12,21 +12,8 @@ public class WinnowStrategy implements PDStrategy{
 
   @Override
   public List<ReportLine> checkPlagiarism(String code1, String code2) {
-    List<ReportLine> l = new ArrayList<>();
-    ReportLine ri = new ReportLine();
-    ri.setModel(Model.WINNOWING.getValue());
-    ri.setScore(ImplementWinnow(code1,code2));
-    l.add(ri);
-    return l;
-  }
-
-  /**
-   * Implement winnow strategy to determine code similarity
-   * @param code1 First code to check
-   * @param code2 Second code to check
-   * @return Similarity Rate between the two codes
-   */
-  private Float ImplementWinnow(String code1, String code2) {
-    return new Winnow(code1, code2).getSimilarity();
+    Winnow ngram = new Winnow(code1, code2);
+    ngram.calcSimilarityRate();
+    return ngram.getReport();
   }
 }
